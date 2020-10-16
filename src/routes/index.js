9d407a80b800ff0fd40e811021c3782ab8aa21be
Antheny Wells <anthenywells@go-away.lan>
2020-10-16 19:18:08 -0400

    custom middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { indexPage } from '../controllers';
 import { messagesPage, addMessage } from '../controllers/messages';
+import { modifyMessage } from '../middleware';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage)
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;

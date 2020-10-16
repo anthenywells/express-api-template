67ddd85a986fca49e2e195f8c8a32303cb67cd11
Antheny Wells <anthenywells@go-away.lan>
2020-10-16 19:20:44 -0400

    async middleware and error handling

--- src/app.js
+++ src/app.js
@@ -9,5 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
+//error handler
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.stack });
+});
 
 export default app;

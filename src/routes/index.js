6fd264d7bc31500c379d8442ce8f1b5887a65283
Antheny Wells <anthenywells@go-away.lan>
2020-10-16 18:20:41 -0400

    postgreSQL database setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { indexPage } from '../controllers';
+import { messagesPage } from '../controllers/messages';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage)
 
 export default indexRouter;
